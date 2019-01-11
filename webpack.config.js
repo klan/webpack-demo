751642155505f46a9d44aeb1d44b8d98485209a7
Klaus Nielsen <klni@hesehus.dk>
2019-01-11 10:17:27 +0100

    Added a basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, '/src/app/index.js'), // webpack entry point. Module to start building dependency graph
+
+    output: {
+        path: path.resolve(__dirname, '/public/'), // folder to store bundle
+        filename: 'bundle.js', // name of bundle
+        publicPath: '/' // public URL of the output directory when referenced in a browser
+    },
+
+    module: { // where we defined file patterns and their loaders
+        rules: []
+    },
+
+    plugins: [ // array of plugins to apply to build chunk
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '/src/public/index.html'),
+            inject: 'body'
+        }),
+
+        // make sure we allow any jquery usages outside of our webpack modules
+        // new webpack.ProvidePlugin({
+        //     $: "jquery",
+        //     jQuery: "jquery",
+        //     'window.$': "jquery",
+        //     'window.jQuery': "jquery"
+        // }),
+
+        // clean dist folder
+        new CleanWebpackPlugin(['./scripts/autogen'], {
+            'verbose': true // write logs to console
+        }),
+
+        new webpack.NoEmitOnErrorsPlugin() // avoid publishing when compilation failed
+    ],
+
+    devServer: {
+        contentBase: './src/public', // source of static assets
+        port: 7700
+    }
+};

507bea5879bdfd152156b3d7c1086e0642937b58
Klaus Nielsen <klni@hesehus.dk>
2019-01-11 12:58:41 +0100

    Enabled compile of sass

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,3 @@
-// import '../style/style.css';
+import '../style/style.scss';
 
 console.log('Hello World');
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,18 @@ module.exports = {
     },
 
     module: { // where we defined file patterns and their loaders
-        rules: []
+        rules: [
+            {
+                test: /\.(sass|scss)$/,
+                use: [{
+                    loader: "style-loader" // injects CSS
+                }, {
+                    loader: "css-loader" // translates CSS into a JS module
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }
+        ]
     },
 
     plugins: [ // array of plugins to apply to build chunk
